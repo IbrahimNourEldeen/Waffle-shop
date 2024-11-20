f70c1d2cf1176589093a17c66f714b096008aeb4
IbrahimNourEldeen <ibrahimnoureldeen11@gmail.com>
2024-11-20 09:11:33 +0200

    first commit

---
+++ assets/JS/Script.js
@@ -0,0 +1,10 @@
+var typed=new Typed(".basicHeader",{
+    strings:["أنا سعيد لرؤيتك على موقعي.","هذا هو مكانك المفضل لتناول الوافل"],
+    typeSpeed:150,
+    backSpeed:150,
+    looped:true
+})
+
+ 
+
+
